674408bd522e39e6c8a2bbb21bbb5458bcd135ca
Liz St <lizstspsi@outlook.com>
2021-06-03 20:51:44 -0300

    componente tema sem consumo de api

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ThemeComponent } from './theme/theme.component';
 
 const routes: Routes = [
   {path:'', redirectTo: 'entrar', pathMatch: 'full'},
 
 {path: 'entrar', component: LoginComponent},
 {path: 'cadastrar', component: RegisterComponent},
-{path: 'inicio', component: HomeComponent}
-
+{path: 'inicio', component: HomeComponent},
+{path: 'tema', component: ThemeComponent}
 ];
 
 @NgModule({
